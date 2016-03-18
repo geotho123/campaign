14ce4e72555cc14b96a3918c6d6cae6709e1d2e9
geotho123 <georgethomasm@gmail.com>
2016-03-18 13:32:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,30 @@ var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
 var routes = require('./routes/index');
 var api = require('./routes/api');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
